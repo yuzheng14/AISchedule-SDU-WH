06bbe5202cc535b8286205f5795eb4f9ab771b54
yuzheng14 <422450117@qq.com>
2021-03-07 20:23:30 +0800

    Update provider.js and README.md

--- provider.js
+++ provider.js
@@ -2,5 +2,6 @@
  * 山东大学（威海）小爱同学课程表provider函数
  */
 function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
+    alert("如课程表导入有问题请加QQ群咨询：653831786")
     return dom.getElementById('kbtable').innerHTML;
 }
